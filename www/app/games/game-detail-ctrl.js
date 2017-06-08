855403ddb514dbb6f99a4bf320ec6d637662d1e9
Doubleshot1122 <skibo1122@yahoo.com>
2017-06-08 12:45:36 -0700

    Date format complete -tried to install moment but it failed to work -manually formated date/time

--- www/app/games/game-detail-ctrl.js
+++ www/app/games/game-detail-ctrl.js
@@ -1,6 +1,6 @@
 (function(){
 
-  angular.module('artemisApp').controller('gameDetailCtrl', ['$state', 'artemisApi', '$stateParams', '$http', '$q', gameDetailCtrl]);
+  angular.module('artemisApp').controller('gameDetailCtrl', ['$state', 'artemisApi', '$stateParams', '$http', '$q',gameDetailCtrl]);
 
   function gameDetailCtrl($state, artemisApi, $stateParams, $http, $q) {
     const vm = this;
@@ -24,6 +24,16 @@
         var gameDetails = results[0].data.result[0];
         var users = results[1].data.result;
         var status = results[2].data.result;
+        var dateYear = gameDetails.start_time.slice(0, 4);
+        var dateMonth = gameDetails.start_time.slice(5, 7);
+        var dateDay = gameDetails.start_time.slice(8, 10);
+        var gameDateFormated = `${dateMonth}/${dateDay}/${dateYear}`;
+        var dateTime = gameDetails.start_time.slice(11, 16);
+        var dateMeridian = getMeridian(dateTime);
+        var gameTimeFormated = `${dateTime} ${dateMeridian}`;
+
+        console.log(gameDateFormated);
+        console.log(gameTimeFormated)
 
         //builds and populates the playerStatus array of objects for single game display
         var playerStatus = []
@@ -41,7 +51,6 @@
           })
         })
 
-        // console.log(playerStatus);
 
       })
 
@@ -50,4 +59,13 @@
     $state.go("app.game-detail");
   };
   }
+
+  function getMeridian(time) {
+    if (time.slice(0, 2) >= 12) {
+      return "PM"
+    } else {
+      return "AM"
+    }
+  }
+
 }());
