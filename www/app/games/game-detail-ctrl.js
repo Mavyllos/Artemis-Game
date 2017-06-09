fe28abfc618fe5346500f342254d2cec78ae5484
Doubleshot1122 <skibo1122@yahoo.com>
2017-06-08 17:42:39 -0700

    removed code for feature that was removed (pull from api for single game view)

--- www/app/games/game-detail-ctrl.js
+++ www/app/games/game-detail-ctrl.js
@@ -8,31 +8,8 @@
     var games = artemisApi.getGames();
     vm.games = games;
 
-    // window.localStorage['gameId'] = 1;
-    // var id = window.localStorage['gameId'];
-
-    // var singleGameObject = {name:"Test name"};
-    // vm.singleGameObject = singleGameObject
-
-    // GameSingle.testFunc().then(function(dataOUT){
-    //   vm.test = dataOUT.result[1].invite_status
-    //   singleGameObject.name = dataOUT.result[1].invite_status
-    //   console.log(dataOUT.result[1].invite_status);
-    //
-    // })
-    //
-    // GameSingle.loadPageWithData().then(function(singleGameDate){
-    //   vm.singleGameObject = singleGameDate
-    //   console.log(singleGameDate);
-    // })
-
-    // console.log(singleGameObject);
-
     vm.selectGame = function(id){
       $state.go("app.game-detail");
     };
   }
-
-
-
 }());
